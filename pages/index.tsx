7d62f36f256d4a5b275d38a8237558c3bc54c1b1
Tomthedeveloper <tomthedeveloper11@gmail.com>
2022-06-19 15:26:48 +0700

    Add Truck function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 import { Truck } from '../types/common';
 import truckBloc from '../lib/truck';
 import { InferGetStaticPropsType, GetStaticPropsContext } from 'next';
@@ -7,6 +8,32 @@ import { InferGetStaticPropsType, GetStaticPropsContext } from 'next';
 export default function Home({
   trucks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [newTruck, setNewTruck] = useState({
+    name: '',
+    imageUrl: '',
+  });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setNewTruck((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  async function addTruck() {
+    const response = await fetch('http://localhost:3000/api/truck', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...newTruck }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <>
       <Head>
@@ -14,6 +41,32 @@ export default function Home({
       </Head>
       <div className="container mx-auto p-5">
         {/* <DataTable<DummyData> data={dummyData} headers={headerConfig} /> */}
+        <h1>Tambah Truck Baru</h1>
+        <form action="post">
+          <div className="form-group m-3">
+            <label className="me-2">Name: </label>
+            <input
+              className="me-2"
+              type="text"
+              value={newTruck.name}
+              name="name"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group m-3">
+            <label className="me-2">ImageURL: </label>
+            <input
+              className="me-2"
+              type="text"
+              value={newTruck.imageUrl}
+              name="imageUrl"
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+        <button type="button" onClick={() => addTruck()}>
+          Add Truck
+        </button>
         <h1>My Trucks</h1>
         <div className="grid grid-cols-4 gap-4">
           {trucks.map((truck) => (
