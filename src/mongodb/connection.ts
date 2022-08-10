b7ffc2035d936069b0e50ed500fba81c58f1ca7d
Tomthedeveloper <tomthedeveloper11@gmail.com>
2022-08-10 21:57:30 +0700

    Change mongoDB connection

--- src/mongodb/connection.ts
+++ src/mongodb/connection.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default async function connectDb() {
   try {
-    const db = await mongoose.connect('mongodb://localhost:27022/trucking');
+    const db = await mongoose.connect(process.env.MONGO_DB_URL);
     console.log('Mongoose Connection Established');
     return db.connection;
   } catch (error) {
