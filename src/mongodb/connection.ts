72a012213d99ed6f49c4e9e9935b3c025a0220f3
Filbert Filbert <fildabert@Filberts-MacBook-Pro.local>
2022-08-10 16:09:03 +0700

    update db connection

--- src/mongodb/connection.ts
+++ src/mongodb/connection.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 export default async function connectDb() {
   try {
-    const db = await mongoose.connect('mongodb://localhost:27017/trucking');
+    const db = await mongoose.connect(
+      process.env.MONGO_DB_URL || 'mongodb://localhost:27017/trucking'
+    );
     console.log('Mongoose Connection Established');
     return db.connection;
   } catch (error) {
