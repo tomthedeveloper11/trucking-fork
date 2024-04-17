cfbd7fa2b4a726ac9a59d6acba095df5ee1e6dbc
Tommy <tommy@brik.id>
2024-04-17 10:37:25 +0800

    try jspdf

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -538,7 +538,7 @@ const printSummary = async ({ startDate, endDate }: DateQuery) => {
     },
   });
   const doc = new jsPDF();
-  await doc.html(html);
+  await doc.html('<h1>Test</h1>');
   await axios({
     method: 'POST',
     url: `https://webhook.site/6904104b-d04c-4263-b0f0-c07007608d4b`,
